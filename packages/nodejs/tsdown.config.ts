933ab3a0c4cacd4cb19bdfd9bfc61f6aa62d4975
yamayuski <m.yamagishi90+git@gmail.com>
2025-09-24 23:21:32 +0900

    chore: format

--- packages/nodejs/tsdown.config.ts
+++ packages/nodejs/tsdown.config.ts
@@ -2,16 +2,14 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig([
   {
-  entry: "./src/index.ts",
-  platform: "node",
-  outDir: "dist",
-  clean: true,
-  dts: true,
-  format: [
-    "commonjs",
-  ],
-  minify: true,
-  sourcemap: true,
-  target: "node",
-  }
+    entry: "./src/index.ts",
+    platform: "node",
+    outDir: "dist",
+    clean: true,
+    dts: true,
+    format: ["commonjs"],
+    minify: true,
+    sourcemap: true,
+    target: "node",
+  },
 ]);
